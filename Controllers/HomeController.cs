f4cc227152f9a5368c7944c408cae266323dbc3c
NOAH DEMIAN ROTBARD <52096691@ort>
2023-05-08 10:24:45 -0300

    guardado 1.0

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,11 +6,6 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        
-        return View();
-    }
-    public IActionResult ListaDestinos()
-    {
         //ORTWorld ortworld = new ORTWorld();
         ViewBag.Diccionario = ORTWorld.DiccionarioPaquetes; 
         return View();
@@ -20,10 +15,15 @@ public class HomeController : Controller
         ViewBag.ListaAereos = ORTWorld.ListaAereos;
         ViewBag.ListaHoteles = ORTWorld.ListaHoteles;
         ViewBag.ListaDestinos = ORTWorld.ListaDestinos;
-        return View();
+        return View(SelectPaquete);
     }
     
-    public IActionResult GuardarPaquete(int Destino, int Hotel, int Aereo, int Excursión){
-        Paquete paquete = new Paquete();
+    public IActionResult GuardarPaquete(int Destino, int Hotel, int Aereo, int Excursión){ 
+        if(Destino == 0 | Hotel == 0 | Aereo == 0 | Excursión == 0){ 
+            ViewBag.MensajeDeError = "NO SE RECIBIERON LOS PARAMETROS CORRECTAMENTE.";
+            return View("SelectPaquete");
+        }// 
+        Paquete paquete = new Paquete(); 
+        
     }
 }
