c5572e749dc1db23eecc626debb83c644acd39fc
NOAH DEMIAN ROTBARD <52096691@ort>
2023-05-08 08:12:57 -0300

    Comienzo TP

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP04_PaquetesTurísticos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Paquete.cs
@@ -0,0 +1,14 @@
+class Paquete{
+
+    public string Hotel{get; private set;}  
+    public string Aereo{get; private set;}
+    public string Excursion {get; private set;} //por que le ponias static a estos?
+    public Paquete (string pHotel, string pAereo, string pExcursion){
+
+        Hotel = pHotel;
+        Aereo = pAereo;
+        Excursion = pExcursion;
+    }
+
+   
+}
