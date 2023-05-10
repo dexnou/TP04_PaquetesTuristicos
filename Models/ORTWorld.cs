5c5b09bd5f848b8721cc2c8d7a7715433e8453c3
NOAH DEMIAN ROTBARD <52096691@ort>
2023-05-10 09:03:19 -0300

    v 4

--- Models/ORTWorld.cs
+++ Models/ORTWorld.cs
@@ -1,10 +1,10 @@
 class ORTWorld{
 
     public static List<string> ListaDestinos {get; private set;} = new List<string> {"OtakuLand", "DragoncinWorld", "PolnareffLand", "PollosHermanos", "MIDWorld", "CPenguinWorld", "MiKasa", "eeeee", "LaTerminal", "PASO 240"  }; // Ahora si, acá se le cargan los 10 destinos. hay que ver el de las fotos de hotel
-    public static List<string> ListaHoteles{get; private set;} = new List<string> {"hotel1", "hotel2", "hotel3", "hotel4", "hotel5", "hotel6", "hotel7", "hotel8", "hotel9", "hotel10"};
-    public static List<string> ListaAereos{get; private set;}
+    public static List<string> ListaHoteles{get; private set;} = new List<string> {"Hotel1", "Hotel2", "Hotel3", "Hotel4", "Hotel5", "Hotel6", "Hotel7", "Hotel8", "Hotel9", "Hotel10"};
+    public static List<string> ListaAereos{get; private set;} = new List<string> {"Aéreo1", "Aéreo2", "Aéreo3", "Aéreo4", "Aéreo5", "Aéreo6", "Aéreo7", "Aéreo8", "Aéreo9", "Aéreo10"};
     public static List<string> ListaExcursiones{get; private set;}
-    public static Dictionary<string, Paquete> DiccionarioPaquetes{get; private set;}
+    public static Dictionary<string, Paquete> DiccionarioPaquetes{get; private set;} = new Dictionary<string, Paquete>();
     public bool IngresarPaquete(string destinoSeleccionado, Paquete paquete)
     {
         if(ListaDestinos.Contains(destinoSeleccionado))
