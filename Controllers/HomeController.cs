40e8cb1df98a5de810f3f5971f228fcdef6d267d
NOAH DEMIAN ROTBARD <52096691@ort>
2023-05-22 07:49:11 -0300

    vFinal 100%

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ public class HomeController : Controller
         Dictionary<string, int> diccionarioFaltantes = new Dictionary<string, int>();
         
         if(Destino > 10 | Destino <= 0 | Hotel > 10 | Hotel <=0 | Aereo > 10 | Aereo <=0 | Excursión > 10 | Aereo<=0){ 
-            ViewBag.MensajeDeError = "NO SE RECIBIERON LOS PARÁMETROS CORRECTAMENTE. FALTAN: "; //falta hacer esto del viewbag
+            ViewBag.MensajeDeError = "NO SE RECIBIERON LOS PARÁMETROS CORRECTAMENTE. FALTAN: "; 
             for(int i = 0; i < 4; i++){
                 if(vectorNum[i] <=0 | vectorNum[i] > 10){
                     diccionarioFaltantes.Add(vectorNombres[i], vectorNum[i]);
@@ -35,14 +35,10 @@ public class HomeController : Controller
         }else{
             Index();
             Paquete paquete = new Paquete(ORTWorld.ListaHoteles[Hotel-1], ORTWorld.ListaAereos[Aereo-1], ORTWorld.ListaExcursiones[Excursión-1]); 
-            bool existe = ORTWorld.IngresarPaquete(ORTWorld.ListaDestinos[Destino-1], paquete); //hay que ver si esto esta bien o mal para que se guarde el paquete con el destino deseado y guardarlo en el viewbag. 
+            bool existe = ORTWorld.IngresarPaquete(ORTWorld.ListaDestinos[Destino-1], paquete); 
             ViewBag.Existe = existe;
             ViewBag.MensajeYaExiste = "Ese destino ya existe. ";
         } 
         return View("Index");
     }
 }
-
-//Falta: 
-// Todo lo visual de cs.   
-// Cambiar el "Ese destino ya existe" para que no aparezca en la consola, sino en la web. 
